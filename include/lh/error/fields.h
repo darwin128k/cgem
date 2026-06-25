45aba8ec3a3f989ed5524434a05d16011b4b7639
Egor Tsyganchuk <darwin128K@gmail.com>
2026-06-25 16:48:23 +0300

    Add error handling structure and field definitions
    - Introduced a new header file `fields.h` to define error fields with `code` and `desc`.
    - Updated `main.cgem` to include a mutable struct for error handling, enhancing the error code management system.
    - Improved overall error reporting capabilities by establishing a clear structure for error codes and descriptions.

---
+++ include/lh/error/fields.h
@@ -0,0 +1,12 @@
+#ifndef LH_ERROR_FIELDS_H
+#define LH_ERROR_FIELDS_H
+
+/**
+ * @param code_type
+ * @param desc_type
+ */
+#define lh_error_fields(code_type, desc_type)                                  \
+    code_type code;                                                            \
+    desc_type desc
+
+#endif /* LH_ERROR_FIELDS_H */

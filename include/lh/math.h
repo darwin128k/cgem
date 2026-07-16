658671530cc54c65710b468196342097730e2b59
Egor Tsyganchuk <darwin128K@gmail.com>
2026-07-16 22:47:50 +0300

    Update mathematical operation macros to ensure calculation correctness and add the @wrap attribute to functions in the math module.

--- include/lh/math.h
+++ include/lh/math.h
@@ -5,29 +5,29 @@
 
 #define lh_math_add_one(a) lh_math_add(a, 1)
 
-#define lh_math_sub(a, b) a - b
+#define lh_math_sub(a, b) ((a) - (b))
 
 #define lh_math_sub_one(a) lh_math_sub(a, 1)
 
-#define lh_math_mul(a, b) a *b
+#define lh_math_mul(a, b) ((a) * (b))
 
-#define lh_math_div(a, b) a / b
+#define lh_math_div(a, b) ((a) / (b))
 
-#define lh_math_mod(a, b) a % b
+#define lh_math_mod(a, b) ((a) % (b))
 
-#define lh_math_neg(a) -a
+#define lh_math_neg(a) (-(a))
 
-#define lh_math_eq(a, b) a == b
+#define lh_math_eq(a, b) ((a) == (b))
 
-#define lh_math_ne(a, b) a != b
+#define lh_math_ne(a, b) ((a) != (b))
 
-#define lh_math_lt(a, b) a < b
+#define lh_math_lt(a, b) ((a) < (b))
 
-#define lh_math_le(a, b) a <= b
+#define lh_math_le(a, b) ((a) <= (b))
 
-#define lh_math_gt(a, b) a > b
+#define lh_math_gt(a, b) ((a) > (b))
 
-#define lh_math_ge(a, b) a >= b
+#define lh_math_ge(a, b) ((a) >= (b))
 
 #define lh_math_min(a, b) lh_math_lt(a, b) ? a : b
 
@@ -41,8 +41,8 @@
 
 #define lh_math_is_negative(v) lh_math_lt(v, 0)
 
-#define lh_math_bit_mask(n) 1u << n
+#define lh_math_bit_mask(n) (1u << (n))
 
-#define lh_math_bit_and(a, b) a &b
+#define lh_math_bit_and(a, b) ((a) & (b))
 
 #endif /* LH_MATH_H */
